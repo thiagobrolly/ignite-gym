876fcde5d3f1d7e7fda4b0b59605f5a39c0dca9c
Thiago Brolly <thiagoex377@gmail.com>
2023-11-06 11:29:05 -0300

    feat: Exibindo nome do usuário e foto padrão

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,13 +1,20 @@
+import { TouchableOpacity } from 'react-native';
 import { Heading, HStack, Icon, Text, VStack } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
+
 import { UserPhoto } from './UserPhoto';
-import { TouchableOpacity } from 'react-native';
+
+import defaultUserPhotoImg from '@assets/userPhotoDefault.png';
+
+import { useAuth } from '@hooks/useAuth';
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
-        source={{ uri: 'https://github.com/thiagobrolly.png' }}
+        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
         alt="Imagem do usuário"
         size={16}
         mr={4}
@@ -17,7 +24,7 @@ export function HomeHeader() {
           Olá,
         </Text>
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          Thiago
+          {user.name}
         </Heading>
       </VStack>
 
